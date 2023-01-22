5751b5058a17ee93bae0b89ecb9c7b73b074f4ee
“Víctor” <victorqueiroz027@gmail.com>
2023-01-21 22:35:11 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("C é bess? O dia já foi incluso kkkk")
+    return
+  }
+
+  alert("adicionado com sucesso!")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("BEREGUEJOHNSON", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("BEREGUEJOHNSON")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
